35165ad055188e74552ae895cb43fc6a7749bbb0
shoukreytom <shoukreytom01@gmail.com>
2020-05-27 18:25:15 +0200

    initial commit

---
+++ notify.py
@@ -0,0 +1,13 @@
+import notify2
+
+
+def main():
+    notify2.init("notify")
+    n = notify2.Notification("Tip", "Don't watch porn videos, It'll damage your brain",
+                             "")
+    n.set_timeout(2000)
+    n.show()
+
+
+if __name__ == '__main__':
+    main()
